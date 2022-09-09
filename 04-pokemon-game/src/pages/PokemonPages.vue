297070a2db7a8b58390fa240c15d7e214fc7ef74
Pablo Hebenstreit Gotelli <pablo2e@yahoo.com>
2022-09-09 15:18:54 +0200

    get and show pokemons

--- 04-pokemon-game/src/pages/PokemonPages.vue
+++ 04-pokemon-game/src/pages/PokemonPages.vue
@@ -1,18 +1,42 @@
 <template>
-    <h1>¿Quién es este pokémon?</h1>
-    
-    <PokemonPicture />
-    <PokemonOptions />
+    <h1 v-if="!pokemon">Espere por favor</h1>
+
+    <div v-else>
+        <h1>¿Quién es este pokémon?</h1>
+        
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"/> <!-- los : para bindear  -->
+        <PokemonOptions :pokemons="pokemonArr"/>
+
+    </div>
 </template>
 
 <script>
     import PokemonPicture from '@/components/PokemonPicture.vue';
     import PokemonOptions from '@/components/PokemonOptions.vue';
 
-export default {
-    name: 'PokemonPages',
-    components: {PokemonPicture, PokemonOptions}
+    import getPokemonOptions from '@/helpers/getPokemonOptions'
 
+    //console.log(getPokemonOptions())
+
+export default {
+    components: {PokemonPicture, PokemonOptions},
+    data() {
+        return {
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false
+        }
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonOptions()
+            const rndInt = Math.floor( Math.random() * 4 )
+            this.pokemon = this.pokemonArr[ rndInt ]
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
+    }
 }
 </script>
 
